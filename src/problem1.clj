fb9697f407f51ae715d027e0e0ff8ce24c07ebba
Javier Roncallo <jroncallo96@gmail.com>
2023-05-16 13:15:45 -0500

    Improve problem 1

--- src/problem1.clj
+++ src/problem1.clj
@@ -1,14 +1,20 @@
 (ns problem1)
 (def invoice (clojure.edn/read-string (slurp "invoice.edn")))
-(defn filter-invoice-items [invoice]
-  (->> invoice
-       (:invoice/items)
-       (filter (fn [item]
-                 (let [taxes (:taxable/taxes item)
-                       has-iva-19 (some #(and (= (:tax/rate %) 19) (= (:tax/category %) :iva)) taxes)
-                       retentions (:retentionable/retentions item)
-                       has-ret-fuente-1 (some #(and (= (:retention/rate %) 1) (= (:retention/category %) :ret_fuente)) retentions)]
-                   (or (and has-iva-19 (not has-ret-fuente-1)) (and (not has-iva-19) has-ret-fuente-1)))))))
+(defn tax-is-iva-19 [{rate :tax/rate category :tax/category}]
+  (and (= rate 19)
+       (= category :iva)
+       ))
+(defn retention-is-ret-fuente-1 [{rate :retention/rate category :retention/category}]
+  (and (= rate 1)
+       (= category :ret_fuente)
+       ))
+(defn valid-invoice [{taxes :taxable/taxes retentions :retentionable/retentions}]
+  (let [has-iva-19 (some tax-is-iva-19 taxes)
+        has-ret-fuente-1 (some retention-is-ret-fuente-1 retentions)]
+    (or (and has-iva-19 (not has-ret-fuente-1)) (and (not has-iva-19) has-ret-fuente-1))))
+(defn filter-invoice-items [{items :invoice/items}]
+  (->> items
+       (filter valid-invoice)))
 (defn -main
   []
   (println (filter-invoice-items invoice))
